f9b7d985a0119404762cacd20c7fcf4b3ecdb60e
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-18 12:22:02 +0000

    up

--- python4.py
+++ python4.py
@@ -7,36 +7,28 @@ app = Flask(__name__)
 # Define o caminho para a pasta onde os arquivos JSON estão localizados
 pasta_dados = os.path.join(os.path.dirname(__file__), "dados")
 
-# Define o número de registros por página
-REGISTROS_POR_PAGINA = 10
+# Define o número de registros por consulta
+REGISTROS_POR_CONSULTA = 10
 
+# Função para ler os arquivos JSON e retornar registros
+def get_registros(arquivo, start, end):
+    path_arquivo = os.path.join(pasta_dados, arquivo)
 
-# Função para ler os arquivos JSON e criar endpoints REST
-def criar_endpoints_para_arquivos_json():
-    for arquivo in os.listdir(pasta_dados):
-        if arquivo.endswith(".json"):
-            nome_endpoint = os.path.splitext(arquivo)[0]
-            path_arquivo = os.path.join(pasta_dados, arquivo)
+    with open(path_arquivo, "r") as f:
+        data = json.load(f)  # Carregar dados do arquivo JSON
 
-            # Define a função para manipular a requisição GET
-            def get_handler(file_path):
-                def handler():
-                    start = int(request.args.get('start', 0))  # Obtém o índice de início da página
-                    end = start + REGISTROS_POR_PAGINA  # Calcula o índice de fim da página
+    return data[start:end]
 
-                    with open(file_path, "r") as f:
-                        data = json.load(f)  # Carregar dados do arquivo JSON
+# Rota para retornar os registros de um arquivo JSON específico dentro de um intervalo
+@app.route('/<arquivo>/consultar', methods=['GET'])
+def consultar_registros(arquivo):
+    start = int(request.args.get('start', 0))  # Obtém o índice de início da consulta
+    end = int(request.args.get('end', start + REGISTROS_POR_CONSULTA))  # Obtém o índice de fim da consulta
 
-                    page_data = data[start:end]  # Obtém os registros para a página atual
-                    return jsonify(page_data)
-                return handler
+    # Obtém os registros para o intervalo especificado do arquivo especificado
+    registros = get_registros(arquivo + ".json", start, end)
 
-            # Adiciona o endpoint REST para o arquivo JSON
-            app.add_url_rule(f"/{nome_endpoint}", endpoint=nome_endpoint, view_func=get_handler(path_arquivo), methods=["GET"])
-
-
-# Chama a função para criar os endpoints REST
-criar_endpoints_para_arquivos_json()
+    return jsonify(registros)
 
 if __name__ == "__main__":
     # Inicia o servidor Flask na porta 8080
